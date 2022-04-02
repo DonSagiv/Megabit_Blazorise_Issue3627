34ce2449ad257c88f36bd6c3de0bfb257be93e43
Ari <DonSagiv@users.noreply.github.com>
2022-04-02 14:17:53 -0400

    Add project files.

---
+++ Megabit_Blazorise_Issue3627/Models/Event.cs
@@ -0,0 +1,7 @@
+﻿namespace Megabit_Blazorise_Issue3627.Models
+{
+    public class Event
+    {
+        public Person? PersonItem { get; set; }
+    }
+}
---
+++ Megabit_Blazorise_Issue3627/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Megabit_Blazorise_Issue3627.Models
+{
+    public class Person
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+    }
+}
